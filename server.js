21b382a7fd4d03431a60231ed84f779968490eeb
aijaz009 <38639256+aijaz009@users.noreply.github.com>
2024-08-12 12:00:24 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,16 +6,16 @@ const jspdf = require('jspdf');
 const upload = multer({ dest: './uploads/' });
 
 app.get('/', (req, res) => {
-  console.log('Serving index.html');
   res.sendFile(__dirname + '/index.html');
 });
 
 app.post('/convert', upload.single('image'), (req, res) => {
+  console.log('Received image:', req.file);
   const image = req.file;
   const pdf = new jspdf();
   pdf.addImage(image.buffer, 'JPEG', 0, 0);
   const pdfData = pdf.output('buffer');
-
+  console.log('Generated PDF:', pdfData);
   res.set("Content-Disposition", `attachment; filename="image.pdf"`);
   res.set("Content-Type", "application/pdf");
   res.send(pdfData);
